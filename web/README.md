e90b93397b1ff882905890959ef56a64ea94a9fe
Gabriel Augusto <gabrielaugusto@mac.local>
2025-02-20 17:29:59 -0300

    first commit: primeiros componentes e introdução ao react/next

---
+++ web/README.md
@@ -0,0 +1,8 @@
+# nlw25-connect
+Primeira Next Level Week do ano de 2025
+
+
+[Notion do projeto](https://efficient-sloth-d85.notion.site/NLW-Connect-337b47bcef1640fc9a536f66dd45d8f1)
+
+[Figma do projeto](https://www.figma.com/community/file/1471119935944492720/nlw-connect-devstage)
+
---
+++ web/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/button";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="text-white">Hello world!</div>
+        <h1 className="text-4xl font-semibold font-heading">Hello World</h1>
+
+      <Button text="Diego"/>
+      <Button text="NLW"/>
+      <Button text="Oi"/>
+
+    </main>
+  );
+}
